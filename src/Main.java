4d5186a792643556b9d245ff45e8b6fede15df36
griadooss <github@zimpics.com>
2019-08-01 10:58:14 +1000

    APP 02 - Added MainFrame
    Moved  code from Main.java to constructor of MainFrame.java leaving just
    the call to MainFrame.java as the code under Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,7 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new JFrame("Hello World");
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frame.setSize(600,500);
-                frame.setVisible(true);
-
+                new MainFrame();
             }
         });
 
---
+++ src/MainFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    //fields
+
+
+    //constructor
+    public MainFrame() {
+        super("MySuperFrame");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(600, 500);
+        setVisible(true);
+    }
+
+
+    //methods
+}
