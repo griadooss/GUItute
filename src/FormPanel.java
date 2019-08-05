ba30d734266eba08c8bc33fe72fd66ea06acea73
griadooss <github@zimpics.com>
2019-08-06 09:28:47 +1000

    Text fileds and labels
    Adds Name label and field and Occupation label and field with OK button
    in readiness for applying gridbaglayout manager.

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -4,8 +4,11 @@ import java.awt.*;
 
 public class FormPanel extends JPanel {
     //fields
-
-
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okBtn;
 
     //constructor
     public FormPanel() {
@@ -13,6 +16,13 @@ public class FormPanel extends JPanel {
         dim.width = 250;
         setPreferredSize(dim);
         System.out.println(dim);
+
+        nameLabel = new JLabel();
+        occupationLabel = new JLabel();
+        nameField = new JTextField(10);
+        occupationField = new JTextField(10);
+        okBtn = new JButton("OKay");
+
 
         Border innerBorder = BorderFactory.createTitledBorder("Add Person");
         Border outerBorder = BorderFactory.createEmptyBorder(0,10,5,10);
