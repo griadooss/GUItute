6bae301064d53cf33a386b2a811cd31782b50442
griadooss <github@zimpics.com>
2019-08-06 09:16:27 +1000

    Borders
    Adds and inner and outer border to a new panel .. FormPanel.

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class FormPanel extends JPanel {
@@ -12,6 +13,10 @@ public class FormPanel extends JPanel {
         dim.width = 250;
         setPreferredSize(dim);
         System.out.println(dim);
+
+        Border innerBorder = BorderFactory.createTitledBorder("Add Person");
+        Border outerBorder = BorderFactory.createEmptyBorder(0,10,5,10);
+        setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 
 
     }
