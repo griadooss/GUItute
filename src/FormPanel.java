7dd4c829e4a9b268a767f2bff11163d2bcea2ccd
griadooss <github@zimpics.com>
2019-08-15 07:48:05 +1000

    Replaces Strings in ListBox with AgeCat class
    Creates a simple utility class AgeCat for matching up IDs with Strings.
    This class is then added to the ListBox in place of just strings. The
    AgeCat class also has two methods to return either just the ID or the
    String itself. Yet to be implemented into the FormEvent class. See next
    commit.

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -12,7 +12,7 @@ public class FormPanel extends JPanel {
     private JTextField occupationField;
     private JButton okBtn;
     private FormListener formListener;
-    private JList<String> ageList;
+    private JList<AgeCategory> ageList;
 
     //constructor
     public FormPanel() {
@@ -28,18 +28,18 @@ public class FormPanel extends JPanel {
         occupationField = new JTextField(10);
 
         //Instantiate List Box
-        ageList = new JList<>();
-        DefaultListModel<String> ageModel = new DefaultListModel<>();
-        ageModel.addElement("Under 18");
-        ageModel.addElement("18 to 65");
-        ageModel.addElement("Over 65");
+        ageList = new JList<AgeCategory>();
+        DefaultListModel<AgeCategory> ageModel = new DefaultListModel<>();
+        ageModel.addElement(new AgeCategory(0, "Under 18"));
+        ageModel.addElement(new AgeCategory(1, "18 to 65"));
+        ageModel.addElement(new AgeCategory(2, "Over 65"));
         ageList.setModel(ageModel);
 
-        ageList.setPreferredSize(new Dimension(110,68));
+        ageList.setPreferredSize(new Dimension(110, 68));
         ageList.setBorder(BorderFactory.createEtchedBorder());
         ageList.setSelectedIndex(1);
 
-        //Instantiate OKay Button
+        /* Instantiate OKay Button */
         okBtn = new JButton("OKay");
         okBtn.addActionListener(new ActionListener() {
             @Override
@@ -47,10 +47,16 @@ public class FormPanel extends JPanel {
                 System.out.println("form button pressed\n");
                 String name = nameField.getText();
                 String occupation = occupationField.getText();
-                //String ageCat = ageList.getSelectedValue();
-                //but this will return a string! What we really need is it to return an index as that is how these list
-                //values would be stored in a database ..some lookup table .. so before adding an extra field to the
-                //FormEvent we need to address this point.
+                /*
+                String ageCat = (String) ageList.getSelectedValue();
+                but this will return a string! What we really need is it to return an index as that is how these list
+                values would be stored in a database ..ie in a lookup table .. so before adding an extra field to the
+                FormEvent we need to address this point.
+
+                So do this. (see AgeCategory inner class below)
+                */
+                AgeCategory ageCat = ageList.getSelectedValue();
+                System.out.println(ageCat.getId() +" " + ageCat.toString());
 
                 FormEvent ev = new FormEvent(this, name, occupation);
 
@@ -64,7 +70,7 @@ public class FormPanel extends JPanel {
 
         //Create borders around panel
         Border innerBorder = BorderFactory.createTitledBorder("Add Person");
-        Border outerBorder = BorderFactory.createEmptyBorder(0,10,5,10);
+        Border outerBorder = BorderFactory.createEmptyBorder(0, 10, 5, 10);
         setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 
         //Set layout manager
@@ -82,14 +88,14 @@ public class FormPanel extends JPanel {
         gc.fill = GridBagConstraints.NONE; //tells components whether to take all the space in a cell or not.
         //important to set these at the start because if left out tend to get problems later.
         gc.anchor = GridBagConstraints.LINE_END; //specifies which side of the cell the control sticks to.
-        gc.insets = new Insets(0,0,0,5); //paints a space around the inside of the cell.
+        gc.insets = new Insets(0, 0, 0, 5); //paints a space around the inside of the cell.
         add(nameLabel, gc);
 
         //Field
         gc.gridx = 1; //next column going from Left to Right
         gc.gridy = 0; //still on the first row of grid
         gc.anchor = GridBagConstraints.LINE_START; //specifies which side of the cell the control sticks to.
-        gc.insets = new Insets(0,0,0,0); //resets the space around the inside of the cell to nil.
+        gc.insets = new Insets(0, 0, 0, 0); //resets the space around the inside of the cell to nil.
         add(nameField, gc);
 
         //////////////// Second Row ////////////////////////////////////////////////
@@ -100,14 +106,14 @@ public class FormPanel extends JPanel {
         gc.gridy = 1;//move down to the second row
         gc.gridx = 0;//reset position to the first column
         gc.anchor = GridBagConstraints.LINE_END; //specifies which side of the cell the control sticks to.
-        gc.insets = new Insets(0,0,0,5);//paints a space around the inside of the cell.
+        gc.insets = new Insets(0, 0, 0, 5);//paints a space around the inside of the cell.
         add(occupationLabel, gc);
 
         //Field
         gc.gridy = 1;//stay on the same row (the second one)
         gc.gridx = 1;//move to the next column position .. col two
         gc.anchor = GridBagConstraints.LINE_START; //specifies which side of the cell the control sticks to.
-        gc.insets = new Insets(0,0,0,0);//resets the space around the inside of the cell to nil.
+        gc.insets = new Insets(0, 0, 0, 0);//resets the space around the inside of the cell to nil.
         add(occupationField, gc);
 
         //////////////// Third Row ////////////////////////////////////////////////
@@ -134,7 +140,31 @@ public class FormPanel extends JPanel {
 
 
     //methods
-    public void setFormListener(FormListener listener){
+    public void setFormListener(FormListener listener) {
         this.formListener = listener;
+    }
+}
+
+//to facilitate parsing an index from the list box instead of a string, make this simple inner class. Then use the class when filling the ageList box.
+// Just a utility class for matching up IDs with Strings. This class is then added to the ListBox in place of strings.
+class AgeCategory {
+    //fields
+    private int id;
+    private String text;
+
+    //constructor
+    public AgeCategory(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    //methods
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public int getId() {
+        return id;
     }
 }
